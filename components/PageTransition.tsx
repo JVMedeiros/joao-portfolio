6b3f8d7062d04a393a1a425b3b1c9fe9870e569c
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-15 21:23:30 -0300

    feat: page transition component

---
+++ components/PageTransition.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation"
+import { ReactNode } from "react"
+
+interface PageTransitionProps {
+  children: ReactNode
+}
+
+export function PageTransition({ children }: PageTransitionProps) {
+  const path = usePathname()
+
+  return (
+    <AnimatePresence>
+      <div key={path}>
+        <motion.div 
+          initial={{ opacity: 1 }} 
+          animate={{ opacity: 0, transition: { delay: 1, duration: 0.4, ease: 'easeInOut' } }} 
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        />
+        {children}
+      </div>
+    </AnimatePresence>
+  )
+}
