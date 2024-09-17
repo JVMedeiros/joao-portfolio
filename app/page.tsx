1012e7b247e5edc6752f0141c30bfe770eda934b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-16 23:10:17 -0300

    feat: home page styles update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import { Photo } from '@/components/Photo';
 import { Social } from '@/components/Social';
 import { Button } from '@/components/ui/button';
 import { FiDownload } from 'react-icons/fi'
@@ -8,7 +9,7 @@ function Home() {
     <section className='h-full'>
       <div className='container mx-auto h-full'>
         <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
-          <div className='text-center xl:text-left'>
+          <div className='text-center xl:text-left order-2 xl:order-none'>
             <span className='text-xl'>Software Engineer</span>
             <h1 className='h1 mb-6'>
               Hello I'm <br/> <span className='text-accent'>João Medeiros</span>
@@ -29,7 +30,9 @@ function Home() {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
