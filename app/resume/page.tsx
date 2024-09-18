e5dd8469cc9f5ae0afd0bb220092e9fa5370f0b6
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-09-17 23:43:42 -0300

    wip: resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,97 @@
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from 'react-icons/fa'
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si'
+
+const about = {
+  title: 'About me',
+  description: 'About me',
+  info: [
+    {
+      fieldName: 'Name',
+      fieldValue: 'João Medeiros'
+    },
+    {
+      fieldName: 'Phone',
+      fieldValue: '+55 12 981045177'
+    },
+    {
+      fieldName: 'Experience',
+      fieldValue: 'Almost 5 years'
+    },
+    {
+      fieldName: 'Email',
+      fieldValue: 'jv.medeiros.gallina@gmail.com'
+    },
+    {
+      fieldName: 'Nationality',
+      fieldValue: 'Brazilian'
+    },
+    {
+      fieldName: 'Freelance',
+      fieldValue: 'Available'
+    },
+    {
+      fieldName: 'Fulltime',
+      fieldValue: 'Available'
+    },
+    {
+      fieldName: 'Language',
+      fieldValue: 'Portuguese, English'
+    },
+  ]
+}
+
+const experience = {
+  icon: '/assets/resume/badge.svg',
+  title: 'My experience',
+  description: 'Description',
+  items: [
+    {
+      company: 'Hospital Israelita Albert Einstein',
+      position: 'Tech Lead and Software Engineer',
+      duration: '2023 - Present'
+    },
+    {
+      company: 'Hospital Israelita Albert Einstein',
+      position: 'Fullstack Software Engineer',
+      duration: '2020 - 2023'
+    },
+    {
+      company: 'Zappts',
+      position: 'Mobile and Frontend Intern',
+      duration: '2020 - 2020'
+    },
+  ]
+}
+
+const education = {
+  icon: '/assets/resume/cap.svg',
+  title: 'My education',
+  description: 'Description',
+  items: [
+    {
+      company: 'FATEC - São José dos Campos',
+      degree: 'System Analysis and Development',
+      duration: '2023 - Present'
+    },
+    {
+      company: 'Rocketseat',
+      degree: 'Backend Bootcamp',
+      duration: '2024'
+    },
+    {
+      company: 'Rocketseat',
+      degree: 'Frontend Bootcamp',
+      duration: '2024 - Present'
+    },
+    {
+      company: 'Rocketseat',
+      degree: 'Golang Bootcamp',
+      duration: '2024 - Present'
+    },
+  ]
+}
 
 function Resume() {
   return (
